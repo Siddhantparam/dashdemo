2702e67c909d80e7abe268df543feb65a16d0cbd
Siddhantparam <145552419+Siddhantparam@users.noreply.github.com>
2024-03-05 10:32:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,7 @@ from pandasai.callbacks import BaseCallback
 from pandasai.llm import OpenAI
 from pandasai.responses.response_parser import ResponseParser
 
-
-class StreamlitCallback(BaseCallback):
-    def __init__(self, container) -> None:
-        """Initialize callback handler."""
-        self.container = container
-
-    def on_code(self, response: str):
-        self.container.code(response)
-
-
+@st.cache
 class StreamlitResponse(ResponseParser):
     def __init__(self, context) -> None:
         super().__init__(context)
@@ -48,7 +39,6 @@ query4 = "Closing balance being lower than 20 is a warining give an alert messag
 query5 = "Plot the recieved quantity for the march of 2022"
 query6 = "recieved below 5 is problem plz give an alert for those days of march 2022"
 
-@st.cache
 if query1:
     llm = OpenAI(api_token='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')
     query_engine = SmartDataframe(
